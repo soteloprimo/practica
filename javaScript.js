72bdc3fb5ff9b98c1ee402983312beea02d0c32a
primo <primosotelo369@gmail.com>
2023-08-04 00:54:15 -0400

    inicio

---
+++ javaScript.js
@@ -0,0 +1,18 @@
+var numerol = 5;
+var  numero2 = 8;
+
+if(numero1>numero2) 
+{
+alert("numerol no es mayor que numero2");
+}
+
+ if(numero2>0) {
+alert("numero2 es positivo");
+}
+
+if(numero1<0) {
+alert("numerol es negativo o distinto de cero");
+}
+ if(numero2>(numero1+1)) {
+alert("Incrementar en 1 unidad el valor de numerol no lo hace mayor o igual que numero2");
+}
